c986b40a0a8086e94b9bb7e10456612ea3ec8c32
spynetS <alfred@stensatter.se>
2022-03-21 21:10:42 +0100

    update

--- main.py
+++ main.py
@@ -6,85 +6,88 @@ import threading
 
 from Shape import Shape
 
-#q 113
-#d 100
-#a 97
-#s 115
-#w 119
-playing = True
-shape1 = Shape("")
-fallenshapes = []
-width = 15
-height = 15
-screen = curses.initscr()
 
-def inputs(screen):
-    global playing
-    global shape1
+class Main:
+    width = 15
+    height = 15
+    screen = curses.initscr()
+    fallenshapes = []
+    playing = True
+    # currentShape = Shape()
+
+
+    def __init__(self):
+        self.screen = curses.initscr()
+        self.currentShape = Shape(self.screen)
+
+    # Game board drawing
+    # I would like to make a board class
+    # but i try to make this program a
+    # bit simple so i just draw the board
+    # in the main class instead
+
+    def drawGround(self):
+        for x in range(self.width):
+            self.screen.addstr(self.height,x,"#")
+
+    def drawWalls(self):
+        for y in range(self.height):
+            self.screen.addstr(y,0,"#")
+
+    def draw(self): #draw everyting
+        self.drawGround()
+        self.drawWalls()
+        for shape in self.fallenshapes:
+            shape.drawShape()
+        self.currentShape.drawShape()
+
+
+    def collide(self):
+        for p in self.currentShape.points:
+            if(p[1]+1 >= self.height):
+                return True
+            for shape in self.fallenshapes:
+                for p1 in shape.points:
+                    if(p[0]==p1[0] and p1[1]==p[1]+1):
+                        return True
+
+        return False
+
+    def land():
+        tempshape = self.currentShape.copy()
+        self.fallenshapes.append(tempshape)
+        self.currentShape = Shape(self.screen)
+
+
+    def main(self):
+
+        while(self.playing):
+            self.screen.erase()#clear screen
+            self.draw()
+            if(self.collide()==False):
+                self.currentShape.y+=1
+            else:
+                self.land()
+            time.sleep(0.5)
+
+def inputs(main):
     while(True):
-        c = screen.getch()
+        c = main.screen.getch()
         if (str(c) == "119"):
-            shape1.rotate(1)
+            main.currentShape.rotate(1)
         if (str(c) == "100"):
-            shape1.x +=1
+            main.currentShape.x +=1
         if (str(c) == "97"):
-            shape1.x -=1
+            main.currentShape.x -=1
         if (str(c) == "115"):
-            shape1.rotate(2)
-
+            main.currentShape.rotate(2)
         if(str(c) == "113"):
-            playing = False
+            main.playing = False
             break
 
-def collide():
-    global height
-    for p in shape1.points:
-        if(p[1]+1 >= height):
-            return True
-        for shape in fallenshapes:
-            for p1 in shape.points:
-                if(p[0]==p1[0] and p1[1]==p[1]+1):
-                    return True
-
-    return False
-
-def drawGround():
-    global screen
-    global height
-    global width
-    for x in range(width):
-        screen.addstr(height,x,"#")
-
-def drawWalls():
-    global screen
-    global height
-    global width
-    for y in range(height):
-        screen.addstr(y,0,"#")
-
-
-screen = curses.initscr()
-
-x = threading.Thread(target=inputs,args=(screen,))
+main = Main()
+x = threading.Thread(target=inputs,args=(main,))
 x.start()
-
-shape1 = Shape(screen)
-
-while(playing):
-    screen.erase()
-    drawGround()
-    drawWalls()
-    shape1.drawShape()
-    for shape in fallenshapes:
-        shape.drawShape()
-
-    if(collide()==False):
-        shape1.y+=1
-    else:
-        tempshape = shape1.copy()
-        fallenshapes.append(tempshape)
-        shape1 = Shape(screen)
-    time.sleep(0.5)
-
+main.main()
 curses.endwin()
 
